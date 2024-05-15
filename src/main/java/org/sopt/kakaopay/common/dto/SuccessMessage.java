25b84f811cdb068c565c796172afcd07cfaaa33b
Yangdaehan <yangdh08@naver.com>
2024-05-15 18:26:01 +0900

    [feat] getPayPoint api 추가

--- src/main/java/org/sopt/kakaopay/common/dto/SuccessMessage.java
+++ src/main/java/org/sopt/kakaopay/common/dto/SuccessMessage.java
@@ -9,9 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(),"블로그 생성이 완료되었습니다."),
-    POST_CREATE_SUCCESS(HttpStatus.CREATED.value(),"게시글 생성이 완료되었습니다."),
 
-    POST_FIND_SUCCESS(HttpStatus.OK.value(), "게시글 찾기가 완료되었습니다.");
+    PAYPOINT_FIND_SUCCESS(HttpStatus.OK.value(), "페이포인트 조회가 완료되었습니다.");
 
     private final int status;
     private final String message;
