d9fd4888ed88d97c2be423d08624fdc25fe5d99d
elive7 <parksub020704@gmail.com>
2024-05-15 17:32:16 +0900

    #4 [feat] getPayMoney api 추가

--- src/main/java/org/sopt/kakaopay/controller/MemberController.java
+++ src/main/java/org/sopt/kakaopay/controller/MemberController.java
@@ -1,13 +1,29 @@
 package org.sopt.kakaopay.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.kakaopay.common.dto.SuccessMessage;
+import org.sopt.kakaopay.common.dto.SuccessStatusResponse;
 import org.sopt.kakaopay.service.MemberService;
+import org.sopt.kakaopay.service.dto.PayMoneyFindDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/vi/member")
+@RequestMapping("api/v1/member")
 public class MemberController {
     private final MemberService memberService;
+
+    @GetMapping("/paymoney")
+    public ResponseEntity<SuccessStatusResponse<PayMoneyFindDto>> getMemberPayMoney(
+            @RequestHeader Long memberId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessStatusResponse.of(SuccessMessage.PAYMONEY_FIND_SUCCESS,
+                        memberService.findPayMoneyById(memberId)));
+    }
 }
